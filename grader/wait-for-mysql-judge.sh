f0189156a8029fef0ac7d301abab5be1b6d63671
apiphoomchu <apiphoom23@gmail.com>
2024-10-24 23:45:22 +0700

    fix

--- grader/wait-for-mysql-judge.sh
+++ grader/wait-for-mysql-judge.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Source RVM and set up Ruby environment
 source /etc/profile.d/rvm.sh
 rvm use 3.2.1 --default
 
@@ -13,7 +12,6 @@ sleep 1
 done
 echo "MySQL is Ready"
 
-# [7/2/2020] Judge Daemon
 while [ -f "/cafe_grader/judge/setup.sh" ];
 do
 echo "Waiting for setup . . ."
@@ -21,10 +19,5 @@ sleep 1
 done
 echo "Environment is ready"
 
-# Execute the grader script with proper Ruby environment
-/bin/bash -l -c "source /etc/profile.d/rvm.sh && cd /cafe_grader/judge/scripts && ruby grader grading queue --err-log"
-
-# For non-SSL purpose
-#rails s -p 3000 -b '0.0.0.0'
-# with-ssl-cert (Sirawit, 8/4/2019)
-# thin -p 3000 --ssl --ssl-key-file /cafe_grader/server.key --ssl-cert-file /cafe_grader/server.crt start 2>&1
+cd /cafe_grader/judge/scripts && \
+exec /bin/bash -l -c "source /etc/profile.d/rvm.sh && ruby grader grading queue --err-log"
